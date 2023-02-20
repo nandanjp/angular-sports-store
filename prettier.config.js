3666176d0397816cefb653313c7870bc3552474f
Nandan <nandan.jp17@gmail.com>
2023-02-19 23:15:17 -0500

    Finishing Creating Models and Features....

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 4,
+  semicolons: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: "always",
+};
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+  <div class="bg-success p-2 text-center text-white">
+  This is SportsStore</div>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sports-store';
+}
